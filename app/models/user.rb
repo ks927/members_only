9126386b336b2383ea441cdceff3711f2dcdbeb5
KSheehan <ksheehan927@gmail.com>
2017-06-08 14:06:37 -0400

    Set up user -> relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     attr_accessor :remember_token
+    has_many :posts
     before_save { email.downcase! }
     validates :name, presence: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
